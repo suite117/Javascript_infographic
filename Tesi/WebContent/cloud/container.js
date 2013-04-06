02a5cfe77cb2b4c109d16e31b191e7ef6ed8f775
alfonso alongi <suite117@gmail.com>
2013-04-06 10:34:19 +0200

    aggiunta jquery mobile

--- Tesi/WebContent/cloud/container.js
+++ Tesi/WebContent/cloud/container.js
@@ -1,3 +1,41 @@
+function createSelect(id, form, options) {
+	form.append('<select name="slider" id="flip-' + id + '" data-role="slider"></select>');
+
+	var select = $("#flip-" + id);
+	for (var i = 0; i < options.length; i++) {
+		select.append('<option value="' + options[i] + '">' + options[i] + '</option>');
+	}
+
+	select.slider();
+}
+
+function radioButton(id, form, options) {
+	form.append('<fieldset id="fieldset-' + id + '" data-role="controlgroup" data-type="horizontal"></fieldset>');
+
+	var fieldset = $("#fieldset-" + id);
+	for (var i = 0; i < options.length; i++) {
+		var inputId = 'radio-choice-' + i + '-fieldset-' + id;
+		fieldset.append('<input type="radio" name="' + inputId + '" id="' + inputId + '" value="' + options[i] + '" />');
+		fieldset.append('<label for="' + inputId + '">' + options[i] + '</label>');
+		
+		$("#" + inputId).on('click', function() {
+			$(this).attr("checked", true);
+			var label = $("#view-container-" + id + " label");
+			if (label.hasClass('ui-btn-active'))
+				label.removeClass("ui-btn-active");
+			else
+				label.addClass('ui-btn-active');
+
+			//fieldset.controlgroup("refresh");
+		});
+	}
+
+	form.trigger("create");
+	//fieldset.controlgroup("refresh");
+	$("#" + 'radio-choice-' + 0 + '-fieldset-' + id).attr("checked", true).checkboxradio("refresh");
+
+}
+
 var containerId = 0;
 function createContainer() {
 
@@ -11,22 +49,10 @@ function createContainer() {
 	$("#views-container").append('<div id="' + divId + '" class="view-container">' + '<div class="view-header"><div class="droppable">&nbsp;</div><div class="view-buttons"><form></form></div></div><div id="view-container-body-' + id + '" class="view-container-body">&nbsp;</div></div>');
 
 	var form = $("#" + divId + " form");
+	var options = ["tutti", "solo selezionati"];
 
-	var options = [["tutti", "ciascuno"], ["non noti"]];
-
-	for (var k = 0; k < options.length; k++) {
-		form.append('<div id="radio-' + k + '"></div>');
-		var radioGroup = $("#view-container-" + id + " #radio-" + k);
-
-		for (var i = 0; i < options[k].length; i++) {
-			radioGroup.append('<input type="radio" id="' + options[k][i] + '-' + id + '" />');
-			radioGroup.append('<label for="' + options[k][i] + '-' + id + '">' + options[k][i] + '</label>');
-
-		}
-
-		radioGroup.buttonset();
-
-	}
+	//radioButton(id, form, options);
+	createSelect(id, form, options);
 
 	// container listener
 	$("#" + divId).data("id", id);
@@ -34,11 +60,8 @@ function createContainer() {
 	$("#" + divId).on("dataChange", function(d, dataSource, param2) {
 		// quando entro qua dentro sono già nel container richiamato dal precedente
 
-		//$("#" + prefix + id + " .view-container-body").text("ciao sono " + id);
-		//var data = $("#" + divId).data("d");
-
 		if (data != null) {
-			var els = data.elements.joinTable(dataSource.elements, "idEvento", "id");
+			var els = data.elements.joinTable(dataSource.elements, "id", "idEvento");
 			console.log(els);
 			data.elements = els;
 			if (els.length != 0)
